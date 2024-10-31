ef4354e9e4036a01e95a8649bcc9a36df7766aea
Kit Zihlman <hnz.co@outlook.com>
2024-10-30 21:10:47 -0600

    Added user input and flavors

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("For real, I hate frozen yogurt.")
+console.log("For real, I have a blood feud against frozen yogurt.")
+
+let userInputOrder = prompt(
+    "Enter your order separated by commas, you filthy froyo-loving degenerate.",
+    "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+);
+
+const flavors = userInputOrder.split(",");
+console.log(flavors)
+
+const order = {
+    swill: ["vanilla", "strawberry", "coffee"]
+}
+
+for (let i = 0; i < flavors.length; i++) {
+    console.log(flavors[i])
+}
